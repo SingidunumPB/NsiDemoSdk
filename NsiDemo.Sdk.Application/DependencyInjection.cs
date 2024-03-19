4675a8d138b38c61e3fe8c4b93fa018fb7170f84
simpel-transport-pb <petarbisevac.pb@outlook.com>
2024-03-19 14:17:08 +0100

    .
    .

---
+++ NsiDemo.Sdk.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NsiDemo.Sdk.Application.Client;
+
+namespace NsiDemo.Sdk.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddNsiDemoSdk(this IServiceCollection services)
+    {
+        services.AddScoped<INsiDemoSdkClient, NsiDemoSdkClient>();
+        return services;
+    }
+}
